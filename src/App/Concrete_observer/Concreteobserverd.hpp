cea3671571f6c5eea1487aa2d0f8db5a7711ac80
motianjie <motianjie@asensing.com>
2023-07-18 10:37:46 +0800

    优化观察者

--- src/App/Concrete_observer/Concreteobserverd.hpp
+++ src/App/Concrete_observer/Concreteobserverd.hpp
@@ -9,24 +9,23 @@ class ConcreteObserver_0 : public Observer
 {
 public:
     ~ConcreteObserver_0();
-
-    ConcreteObserver_0& operator=(const ConcreteObserver_0&) = delete;
-    ConcreteObserver_0(const ConcreteObserver_0&) = delete;
-
     static std::shared_ptr<ConcreteObserver_0> get_instance()
     {
         static std::shared_ptr<ConcreteObserver_0> instance(new ConcreteObserver_0());
         return instance;
     }
-
     void Notify(uint16_t DID, void* data,unsigned int len);
+
 private:
-    std::string m_name;
     ConcreteObserver_0()
     {
         m_name="ConcreteObserver";
         spdlog::info("ConcreteObserver default Constructor");
     }
+    ConcreteObserver_0& operator=(const ConcreteObserver_0&) = delete;
+    ConcreteObserver_0(const ConcreteObserver_0&) = delete;
+private:
+    std::string m_name;
 };
 
 #endif //
